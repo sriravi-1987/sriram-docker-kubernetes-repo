89a5950df8e70242337f17b47b59224b1233b3cf
sriram ravi <sriram.ravi1002@gmail.com>
2026-07-20 00:11:27 +0530

    pushing latest changes

--- ShopSphere.Client/src/app/core/constants/api.constants.ts
+++ ShopSphere.Client/src/app/core/constants/api.constants.ts
@@ -1,3 +1,3 @@
 export const API_ENDPOINTS = {
-  products: 'http://localhost:8080/api/products'
+  products: 'http://a6799637ce2c44edabf66b7c97e67e4a-488897649.eu-north-1.elb.amazonaws.com/api/products'
 };
